50bb6a0a3d5e6774bf2064a79ebba7c269566b73
Dan <dan@overthinker.net>
2021-01-10 16:05:44 -0600

    Attempting to get a play()-based sound right

--- pykroz/make-wav.py
+++ pykroz/make-wav.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import wave
 
 
-BITRATE = 44100 # per ms, not per second
+BITRATE = 88200 # per ms, not per second
 
 def sine(frequency = 440.0, duration = 1.0) -> bytes:
     points = int(BITRATE * duration)
@@ -17,10 +17,6 @@ def sine(frequency = 440.0, duration = 1.0) -> bytes:
     data = cast(recarray, a)
     return data.tobytes()
 
-def durationOfInstruction(instr: Tuple[float, float]):
-    (_, duration) = instr
-    return duration
-
 def wav(filename: str, instructions: List[Tuple[float, float]]):
     frames = bytes(reduce(
         lambda soFar, current: [*soFar, *sine(*current)],
@@ -31,10 +27,10 @@ def wav(filename: str, instructions: List[Tuple[float, float]]):
     with open(filename, 'wb') as f:
         with wave.open(f) as file:
             wfile = cast(wave.Wave_write, file)
-            # totalDuration = sum(map(durationOfInstruction, instructions))
             wfile.setframerate(BITRATE)
             wfile.setnchannels(1)
             wfile.setsampwidth(1)
             wfile.writeframes(frames)
 
-wav('pykroz/sound/title-prompt.wav', [(220, 0.1)])
+wav('pykroz/sound/title-prompt.wav', [(200, 0.1)])
+wav('pykroz/sound/resume-game.wav', [(500, 0.1), *[(f, 1/30) for f in range(200, 100, -1)], (100, 0.1)])
