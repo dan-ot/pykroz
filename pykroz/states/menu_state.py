afce6e5e11ec228ae1ed8ecf04b1b8d0c0686dfe
Dan <dan@overthinker.net>
2021-04-13 08:31:47 -0500

    Thinking through what a Page is and how it works

--- pykroz/states/menu_state.py
+++ pykroz/states/menu_state.py
@@ -1,8 +1,21 @@
+from typing import Callable, MutableMapping, Optional
 from pygame import Rect, Surface
 
 from states.protocols import CommandableState
 from commands import Command
 from engine.ascii import ASCII
+
+class Page():
+    def __init__(self, content: Surface, report_selection: Callable[[Command], None]):
+        self.content = content
+        self.report_selection = report_selection
+
+class PageSet():
+    def __init__(self, pages: list[Page], parent: Optional['PageSet'] = None, data: MutableMapping[str, Command] = {}, current_page: int = 0):
+        self.parent = parent
+        self.data = data
+        self.pages = pages
+        self.current_page = current_page
 
 class MenuState():
     def __init__(self, initialState, font: ASCII, size: Rect):
